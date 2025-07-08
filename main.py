d2a642ffb12c777e44f3b764a8731458bef8f1b6
Liamw <l0i4a1m5@gmail.com>
2025-07-08 18:07:38 +0900

    fixed bug

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ argparser.add_argument("--init", action = "store_true")
 argparser.add_argument("--uninit", action = "store_true")
 argparser.add_argument("--makealt", type=str, default=None)
 argparser.add_argument("--switchalt", type=str, default=None)
-argparser.add_argument("--deletealt", default = None)
-argparser.add_argument("--update", type=str, default=None)
+argparser.add_argument("--deletealt", action = "store_true")
+argparser.add_argument("--update", action = "store_true")
+argparser.add_argument("--alts", action = "store_true")
 
 
 args = argparser.parse_args()
@@ -33,14 +34,18 @@ class Error:
     def __init__(self, error, details):
         self.error = error
         self.errordetail = details
+        
     def stateandstore(self):
         self.errorlog()
         self.storeerror()
+        
     def errorlog(self):
         print("ERROR: " + "'" + self.error + "': "+ self.errordetail)
+        
     def storeerror(self):
         global errorlogs
         errorlogs[f"{self.error}:{len(errorlogs)+1}"] = self.errordetail
+        
     def graberror(self, option):
         global errorlogs
 
@@ -53,6 +58,7 @@ class Error:
             except IndexError as IE:
                 _e_IE = Error("IndexError", "The index user has given is not in the errorlog")
                 _e_IE.stateandstore()
+
 
 ###############
 #	File management
@@ -79,17 +85,31 @@ class Filemanager:
         else:
             os.mkdir(path)
 
-            self.updatealt(alternativename)
+            with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
+                f.write(f"CurrentBranch:{os.path.basename(path)}")
+                
+            self.updatealt()
     
-    def updatealt(self, altname):
+    def updatealt(self):
+        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
+            altname = f.read().split(":")[1]
+        
         path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
         for file in os.listdir(os.getcwd()):
             filepath = os.path.join(os.getcwd(), file)
 
             if os.path.isfile(filepath):
                 shutil.copy(filepath, path)
+        
+        for file in os.listdir(path):
+            if not file in os.listdir(os.getcwd()):
+                os.remove(os.path.join(path, file))
 
     def switchalt(self, altname):
+        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
+            f.write(f"CurrentBranch:{altname}")
+        
+        
         path = os.path.join(os.getcwd())
         for file in os.listdir(os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)):
             filepath = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname, file)
@@ -97,10 +117,15 @@ class Filemanager:
             if os.path.isfile(filepath):
                 shutil.copy(filepath, path)
 
-    def deletealt(self, altname):
+    def deletealt(self):
+        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
+            altname = f.read().split(":")[1]
+            
         if not altname == "main":
             path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
             shutil.rmtree(path)
+            with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
+                f.write(f"CurrentBranch:main")
         else:
             _e_DME = Error("Delete Main Error", "User tried to delete main alt!")
             _e_DME.stateandstore()
@@ -110,16 +135,26 @@ class Filemanager:
             shutil.rmtree(os.path.join(os.getcwd(), f"{self.projectname}_AK"))
         else:
             pass
-
+    
+    def showalts(self):
+        path = os.path.join(os.getcwd(), f"{self.projectname}_AK")
+        for thing in os.listdir(path):
+            if not os.path.isfile(os.path.join(path, thing)):
+                print("-> ", thing)
+                
 ###############
 #	Main
 ###############
+prjNAME = os.path.basename(os.getcwd())
 
-filemanager = Filemanager(os.path.basename(os.getcwd()))
+filemanager = Filemanager(prjNAME)
 
 if args.init == True:
     filemanager.initAK()
     filemanager.makealt("main")
+    
+    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "w") as f:
+        f.write(f"CurrentBranch:main")
 
 if args.uninit == True:
     filemanager.uninit()
@@ -127,11 +162,14 @@ if args.uninit == True:
 if args.makealt != None:
     filemanager.makealt(args.makealt)
 
-if args.update != None:
-    filemanager.updatealt(args.update)
+if args.update == True:
+    filemanager.updatealt()
 
 if args.switchalt != None:
     filemanager.switchalt(args.switchalt)
 
-if args.deletealt != None:
-    filemanager.deletealt(args.deletealt)
+if args.deletealt == True:
+    filemanager.deletealt()
+
+if args.alts == True:
+    filemanager.showalts()
