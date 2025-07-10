b0351da2dbdd898490d8385ada8ca4b7330d9e3e
Liamw <l0i4a1m5@gmail.com>
2025-07-10 17:42:20 +0900

    Ready for beta 1.0.0

--- main.py
+++ main.py
@@ -1,82 +1,63 @@
-###############
+####################################
 #	Imports
-###############
+####################################
+
 import argparse
 import os
 import shutil
 
 
 
-#####################
-#           argparse
-#####################
+####################################
+#	argparse setup
+####################################
+
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--init", action = "store_true")
 argparser.add_argument("--uninit", action = "store_true")
+
 argparser.add_argument("--makealt", type=str, default=None)
 argparser.add_argument("--switchalt", type=str, default=None)
-argparser.add_argument("--desc", type=str, default=None)
-argparser.add_argument("--showdesc", action = "store_true")
 argparser.add_argument("--deletealt", action = "store_true")
 argparser.add_argument("--update", action = "store_true")
 argparser.add_argument("--official", action = "store_true")
-argparser.add_argument("--alts", action = "store_true")
 
+argparser.add_argument("--desc", type=str, default=None)
+argparser.add_argument("--showdesc", action = "store_true")
+argparser.add_argument("--alts", action = "store_true")
 
 args = argparser.parse_args()
 
-#archived
-#print(f"Passed in arguments: , {args.initsetup} ,{args.makealt} ,{args.switchalt} ,{args.deletealt} ,{args.update}")
 
-###############
-#	Error handle
-###############
-errorlogs = {}
+####################################
+#	error class
+####################################
 
 class Error:
-    def __init__(self, error, details):
+    def __init__(self, error, resolve):
         self.error = error
-        self.errordetail = details
+        self.resolve = resolve
         
-    def stateandstore(self):
-        self.errorlog()
-        self.storeerror()
-        
-    def errorlog(self):
-        print("ERROR: " + "'" + self.error + "': "+ self.errordetail)
-        
-    def storeerror(self):
-        global errorlogs
-        errorlogs[f"{self.error}:{len(errorlogs)+1}"] = self.errordetail
-        
-    def graberror(self, option):
-        global errorlogs
-
-        if option == "ALL":
-            for log in errorlogs:
-                print("ERROR: ", log)
-        else:
-            try:
-                print(list(errorlogs)[int(option)])
-            except IndexError as IE:
-                _e_IE = Error("IndexError", "The index user has given is not in the errorlog")
-                _e_IE.stateandstore()
+    def __call__(self):
+        print(f"ERR: '{self.error}' => '{self.resolve}'")
+        print("")
 
 
-###############
-#	File management
-###############
+####################################
+#	Filemanager class
+####################################
+
 class Filemanager:
-    
     def __init__(self, projectname):
         self.projectname = projectname
 
     def initAK(self):
         if not os.path.exists(f"{self.projectname}_AK"):
             os.mkdir(f"{self.projectname}_AK")
+            
         else:
-            _e_FEE = Error("File Exist Error", "The folder that user has tried to init alt keeper already exists!")
-            _e_FEE.stateandstore()
+            e = Error("AK folder exists", "use --uninit or keep the folder")
+            e()
 
     def makealt(self, alternativename):
 
@@ -84,72 +65,101 @@ class Filemanager:
         path = os.path.join(cwd, f"{self.projectname}_AK", alternativename)
 
         if os.path.exists(path):
-            _e_FEE = Error("Folder Exists Error", "The folder that user has tried to make a alt already exists!")
-            _e_FEE.stateandstore()
+            e = Error("Alternative already exists", "Use the alternative or do --deletealt on the alternative wanted")
+            e()
+        
         else:
             os.mkdir(path)
 
-            with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
-                f.write(f"CurrentBranch:{os.path.basename(path)}")
-                
-            self.updatealt()
+            try:
+                with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
+                    f.write(f"CurrentBranch:{os.path.basename(path)}")
+                self.updatealt()
+            except Exception as e:
+                e = Error("Internal file loading Failed", "Do --uninit and then do --init")
+                e()
+                        
     
-      
     def updatealt(self):
-        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
-            altname = f.read().split(":")[1]
-        
-        path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
-        for file in os.listdir(os.getcwd()):
-            filepath = os.path.join(os.getcwd(), file)
+        try:
+            with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
+                altname = f.read().split(":")[1]
+            
+                path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
+            for file in os.listdir(os.getcwd()):
+                filepath = os.path.join(os.getcwd(), file)
 
-            if os.path.isfile(filepath):
-                shutil.copy(filepath, path)
+                if os.path.isfile(filepath):
+                    shutil.copy(filepath, path)
+            
+            for file in os.listdir(path):
+                if not file in os.listdir(os.getcwd()):
+                    os.remove(os.path.join(path, file))
+                    
+        except Exception as e:
+            e = Error("Internal file loading Failed", "Do --uninit and then do --init")
+            e()
         
-        for file in os.listdir(path):
-            if not file in os.listdir(os.getcwd()):
-                os.remove(os.path.join(path, file))
+        
 
     def switchalt(self, altname):
-        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
-            f.write(f"CurrentBranch:{altname}")
-        
-        
-        path = os.path.join(os.getcwd())
-        for file in os.listdir(os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)):
-            filepath = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname, file)
-
-            if os.path.isfile(filepath):
-                shutil.copy(filepath, path)
-
-    def deletealt(self):
-        with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
-            altname = f.read().split(":")[1]
-            
-        if not altname == "main":
-            path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
-            shutil.rmtree(path)
+        try:
             with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
-                f.write(f"CurrentBranch:main")
-        else:
-            _e_DME = Error("Delete Main Error", "User tried to delete main alt!")
-            _e_DME.stateandstore()
-    
+                f.write(f"CurrentBranch:{altname}")
+            
+            
+            path = os.path.join(os.getcwd())
+            for file in os.listdir(os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)):
+                filepath = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname, file)
+
+                if os.path.isfile(filepath):
+                    shutil.copy(filepath, path)
+        
+        except Exception as e:
+            e = Error("Internal file load error", "Do --uninit then --init")
+            e()
+            
+    def deletealt(self):
+        try:
+            with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "r") as f:
+                altname = f.read().split(":")[1]
+                
+            if not altname == "main":
+                path = os.path.join(os.getcwd(), f"{self.projectname}_AK", altname)
+                shutil.rmtree(path)
+                with open(os.path.join(os.getcwd(), f"{self.projectname}_AK", "currentbranch.txt"), "w") as f:
+                    f.write(f"CurrentBranch:main")
+            else:
+                e = Error("User tried to delete main alt", "Do not delete the main alternative")
+                e()
+        except Exception as e:
+            e = Error("Internal file load error","Do --uninit then --init")
+            e()
+            
     def uninit(self):
         if input("Are you sure?:[Y/N]").lower() == "y":
-            shutil.rmtree(os.path.join(os.getcwd(), f"{self.projectname}_AK"))
+            try:
+                shutil.rmtree(os.path.join(os.getcwd(), f"{self.projectname}_AK"))
+            except Exception as e:
+                e = Error("Alt keeper folder does not exist in this directory", "Make one by --init")
+                e()
         else:
             pass
     
     def showalts(self):
-        path = os.path.join(os.getcwd(), f"{self.projectname}_AK")
-        for thing in os.listdir(path):
-            if not os.path.isfile(os.path.join(path, thing)):
-                print("-> ", thing)
-                
-###############
+        try:
+            path = os.path.join(os.getcwd(), f"{self.projectname}_AK")
+            for thing in os.listdir(path):
+                if not os.path.isfile(os.path.join(path, thing)):
+                    print("-> ", thing)
+        except Exception as e:
+            e = Error("Alt keeper folder does not exist in this directory", "Make one by --init")
+            e()
+                    
+####################################
 #	Main
-###############
+####################################
+
 prjNAME = os.path.basename(os.getcwd())
 
 filemanager = Filemanager(prjNAME)
@@ -181,22 +191,39 @@ if args.alts == True:
     filemanager.showalts()
 
 if args.desc != None:
-    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
-        alt = f.read().split(":")[1]
-    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt, "description.txt"), "w") as f:
-        f.write(args.description)
-
+    try:
+        with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
+            alt = f.read().split(":")[1]
+        with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt, "description.txt"), "w") as f:
+            f.write(args.description)
+    except Exception as e:
+        e = Error("Internal file load error", "do --uninit then --init")
+        e()
     
 if args.showdesc == True:
-    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
-        alt = f.read().split(":")[1]
-    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt, "description.txt"), "r") as f:
-        print("->",f.read())
-
+    try:
+        with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
+            alt = f.read().split(":")[1]
+        with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt, "description.txt"), "r") as f:
+            print("->",f.read())
+    except Exception as e:
+        e = Error("Internal file load error", "do --uninit then --init")
+        e()
+        
 if args.official == True:
-    with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
-        alt = f.read().split(":")[1]
-    
-    shutil.rmtree(os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", "main")))
-    shutil.copytree(os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt)), os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", "main")))
-    filemanager.switchalt("main")
+    try:
+        with open(os.path.join(os.getcwd(), f"{prjNAME}_AK", "currentbranch.txt"), "r") as f:
+            alt = f.read().split(":")[1]
+        
+        try:
+            shutil.rmtree(os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", "main")))
+            shutil.copytree(os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", alt)), os.path.join(os.path.join(os.getcwd(), f"{prjNAME}_AK", "main")))
+            filemanager.switchalt("main")
+        except Exception as e:
+            e = Error("Main alternative does not exist", "Make main again by doing --makealt main or do --uninit then --init")
+            e()
+            
+    except Exception as e:
+        e = Error("Internal file load error", "do --uninit then --init")
+        e()
+        
